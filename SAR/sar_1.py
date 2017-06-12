0787d6744c10a2b4cf577d7fae92f98142f56606
quaner <974807643@qq.com>
2017-06-12 18:30:12 +0800

    init

--- SAR/sar_1.py
+++ SAR/sar_1.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun  5 20:00:44 2017
-
-@author: quan
-"""
-
 class getimages:
     def __init__(self,dir,size):
         self.dir = dir
@@ -15,7 +7,7 @@ class getimages:
         import os
         import numpy as np
         import cv2
-        matrix = np.empty((self.size, 1, 256, 256), dtype="float32")
+        matrix = np.empty((self.size, 1, 64, 64), dtype="float32")
         label = np.empty((self.size,), dtype="uint8")
         count = 0
         for j in range(len(self.dir)):
@@ -24,7 +16,7 @@ class getimages:
             imageList = os.listdir(self.dir[j])
             for i in range(len(imageList)):
                 img = cv2.imread(self.dir[j]+'/'+imageList[i],0)
-                res = cv2.resize(img, (256, 256), interpolation=cv2.INTER_CUBIC)
+                res = cv2.resize(img, (64, 64), interpolation=cv2.INTER_CUBIC)
                 arr = np.asarray(res, dtype="float32")
                 # arr -= np.mean(arr)
                 matrix[count, :, :, :] = arr
@@ -38,7 +30,6 @@ from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.normalization import BatchNormalization
 from keras.optimizers import RMSprop
 
 from sklearn.cross_validation import train_test_split
@@ -51,26 +42,26 @@ y_train = np_utils.to_categorical(y_train, 3)
 
 #生成一个model
 model = Sequential()
-model.add(BatchNormalization(axis=1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones',input_shape = (1,256,256)))
-model.add(Convolution2D(32,(3,3),strides=(2,2),padding='valid',data_format='channels_first'))
+
+model.add(Convolution2D(128,(3,3),strides=(2,2),padding='valid',data_format='channels_first',input_shape = (1,64,64)))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2),data_format='channels_first'))  #64*64
 
-model.add(Convolution2D(64,(3,3),strides=(1,1),padding='same',data_format='channels_first'))
+model.add(Convolution2D(256,(2,2),strides=(1,1),padding='same',data_format='channels_first'))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2),data_format='channels_first')) #32*32
 
-model.add(Convolution2D(64,(3,3),strides=(1,1),padding='same',data_format='channels_first'))
+model.add(Convolution2D(256,(2,2),strides=(1,1),padding='same',data_format='channels_first'))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2),data_format='channels_first')) #16*16
 
 model.add(Flatten())
 model.add(Dense(50))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.6))
 model.add(Dense(50))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.6))
 model.add(Dense(3))
 model.add(Activation('softmax'))
 
@@ -85,12 +76,21 @@ model.compile(optimizer=rmsprop,
 model.summary()
 
 print('Training ------------')
-model.fit(X_train, y_train, batch_size=20,validation_split=0.4,epochs=200)
+model.fit(X_train, y_train, batch_size=20,validation_split=0.2,epochs=250)
 
-# config = model.get_config()
+# print('\nTesting ------------')
+# # Evaluate the model with the metrics we defined earlier
+# loss, accuracy = model.evaluate(X_test, y_test)
+#
+# print('test loss: ', loss)
+# print('test accuracy: ', accuracy)
+config = model.get_config()
 
-# import h5py
-# from keras.models import model_from_json
-# json_string = model.to_json()
-# open('add NB.json','w').write(json_string)
-# model.save_weights('add NB.h5')
+import h5py 
+from keras.models import model_from_json  
+json_string = model.to_json()  
+open('my_model_architecture.json','w').write(json_string)  
+model.save_weights('my_model_weights.h5')  
+# #读取model  
+#model = model_from_json(open('my_model_architecture.json').read())  
+#model.load_weights('my_model_weights.h5')  
---
+++ SAR/sar_2.py
@@ -0,0 +1,96 @@
+def getimages(dir,size):
+    import os
+    import numpy as np
+    import cv2
+    matrix = np.empty((size, 1, 64, 64), dtype="float32")
+    label = np.empty((size,), dtype="uint8")
+    count = 0
+    for j in range(len(dir)):
+        assert os.path.exists(dir[j])
+        assert os.path.isdir(dir[j])
+        imageList = os.listdir(dir[j])
+        for i in range(len(imageList)):
+            img = cv2.imread(dir[j] + '/' + imageList[i], 0)
+            res = cv2.resize(img, (64, 64), interpolation=cv2.INTER_CUBIC)
+            arr = np.asarray(res, dtype="float32")
+            matrix[count, :, :, :] = arr
+            label[count] = int(j)
+            count += 1
+    return matrix,label
+
+from sklearn.cross_validation import train_test_split
+
+train, label = getimages([r"/media/quan/软件/AAAAA/SAR/BulkCarrier",r"/media/quan/软件/AAAAA/SAR/ContainerShip",r"/media/quan/软件/AAAAA/SAR/OilTanker"],250)
+X_train, X_test, y_train, y_test = train_test_split(train, label, test_size=0.2, random_state=42) #X_train shape(200, 1, 256, 256)
+
+
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD, Adadelta, Adagrad,RMSprop
+from keras.utils import np_utils, generic_utils
+from keras.layers.normalization import BatchNormalization
+
+train_datagen = ImageDataGenerator(
+                rotation_range=120,
+                width_shift_range=0.2,
+                height_shift_range=0.2,
+                shear_range=0.2,
+                zoom_range=0.2,
+                horizontal_flip=True,
+                data_format='channels_first',
+                fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1./255,data_format='channels_first')
+
+# label为1~3共3个类别，keras要求格式为binary class matrices,转化一下格式
+y_train = np_utils.to_categorical(y_train, 3)
+y_test = np_utils.to_categorical(y_test, 3)
+
+train_generator = train_datagen.flow( X_train,y_train,save_to_dir ='/media/quan/软件/AAAAA/SAR/dir/train',)
+
+validation_generator = test_datagen.flow(X_test,y_test,save_to_dir ='/media/quan/软件/AAAAA/SAR/dir/test')
+
+#生成一个model
+model = Sequential()
+model.add(BatchNormalization(axis=1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones',input_shape = (1,64,64)))
+model.add(Convolution2D(128, (3,3), strides=(2,2),data_format='channels_first'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2),data_format='channels_first'))   #37*37
+
+model.add(BatchNormalization(axis=1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones'))
+model.add(Convolution2D(256, (2,2), strides=(1,1),data_format='channels_first'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2),data_format='channels_first'))  #18*18
+
+model.add(Convolution2D(256, (2,2), strides=(1,1),padding='same',data_format='channels_first'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2),data_format='channels_first'))  #9*9
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(50))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(50))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(3))
+model.add(Activation('softmax'))
+
+# Another way to define your optimizer
+rmsprop = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+# We add metrics to get more results you want to see
+model.compile(optimizer=rmsprop,
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=20,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=20)
